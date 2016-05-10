8503a41ef0add4b9b68e964c5321a7a8b34e59e0
Guillaume Degermann <guillaume.deger@gmail.com>
2016-05-10 15:24:03 +0200

    Complete app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,40 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(user_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(user_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def user_params
+    params.require(:task).permit(:name, :status)
+  end
 end
